89577908d9c7c4237338a5b6e5306a6ae16c5dfa
Dusty211 <kylebenz@hotmail.com>
2019-04-03 15:57:16 -0400

    basic controller and route config. Api is accessible on localhost.

--- app/controllers/api/v1/npcs_controller.rb
+++ app/controllers/api/v1/npcs_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::NpcsController < ApplicationController
+
+  def index
+    render json: Npc.all
+  end
+  
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    render json: User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
