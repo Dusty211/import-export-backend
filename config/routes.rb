f60fe387b3687b6bc2b39bd05b6fb3105189d42b
Dusty211 <kylebenz@hotmail.com>
2019-04-08 13:34:28 -0400

    changed routes to support basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  # pre auth:
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :npcs, only: [:index]
+  #     resources :users, only: [:show, :create] do
+  #       resources :gamestates, only: [:index, :show, :create, :update]
+  #     end
+  #   end
+  # end
+
   namespace :api do
     namespace :v1 do
-      resources :npcs, only: [:index]
-      resources :users, only: [:show, :create] do
-        resources :gamestates, only: [:index, :show, :create, :update]
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
       end
     end
   end
