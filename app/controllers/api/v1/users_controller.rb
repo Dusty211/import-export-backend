47a52d834d1844136d5e21335f1ce93fbc5c8406
Dusty211 <kylebenz@hotmail.com>
2019-04-08 15:52:14 -0400

    jwt token working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::UsersController < ApplicationController
 
+  skip_before_action :authorized, only: [:create]
+
+
   def show
     render json: User.find(params[:id])
   end
@@ -12,7 +15,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
