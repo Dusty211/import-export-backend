3d520260f4b2da74cc514a2223ea50e7b7599fd6
Dusty211 <kylebenz@hotmail.com>
2019-04-16 17:59:01 -0400

    expanded seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -76,8 +76,8 @@ end
 npcs = [
   {name: "Tom Swanson", min_streetcred: 0, max_streetcred: 70, npc_karma: 70},
   {name: "Jon Smith", min_streetcred: 0, max_streetcred: 70, npc_karma: 70},
-  # {name: "Discheveled Person", min_streetcred: 20, max_streetcred: 85, npc_karma: 50},
-  # {name: "Sort of Shady Person", min_streetcred: 30, max_streetcred: 100, npc_karma: 30},
+  {name: "Lorp Tisdale", min_streetcred: 20, max_streetcred: 85, npc_karma: 50},
+  {name: "Darden Pongo", min_streetcred: 30, max_streetcred: 100, npc_karma: 30},
   {name: "Big Cruz", min_streetcred: 50, max_streetcred: 100, npc_karma: 10},
   {name: "Little Cruz", min_streetcred: 50, max_streetcred: 100, npc_karma: 10},
 ]
@@ -86,28 +86,26 @@ allNpcs = npcs.map {|npc| Npc.create(npc)}
 
 bananas = "Hey there old buddy! *Does this guy know me from somewhere?* I have a bunch of bananas I need to import, and you look like the type of individual who wants to do business! Whadda'ya say?"
 logging_equip = "Hey there sport! Could you get this logging equipment to where it needs to go?"
-steel_pipes = "Hey friend, I need to get these pipes up to Alaska stat. Are you the one for the job?"
-tractors = "Howdy partner... These tractors need to get accross the ocean, and I cant't drive them there!  Do we have a deal?"
-stereo_equipment = "Hey bud, I have some stereo equipment that I got for cheap at a garage sale. I need to get it back to my family overseas. It's about 4 containers worth. Are you in?"
+stereo_equipment = "Hey bud, I have some stereo equipment that I got for cheap at a garage sale. I need to get it back to my family overseas. It's about 4 containers worth. They really like stereos. Are you in?"
 eighties_memorabilia = "Hey guy, I have a bunch of vhs players, cassette tapes, and some props from the movie Scarface. You wanna ship it?"
 car_parts = "Wanna make some nice side cash? All I need is for you to get these donated car parts where they need to go..."
 jet_skis = "Hey bro, I'm trying to get these jet skis on the water fast. They're from an... estate sale. Yeah. I got them for real cheap. Are you in?"
 duffel_bags = "I need to get a container of rare designer duffel bags to my overseas location. I heard you're the best at this sort of thing..."
+expedition = 'Hey old chap! I have some of the most elaborate and exclusive treasure and loot. These are for my private collection and I need to get them overseas for a special museam event.'
 
 jobs = [
   {npc_id: allNpcs[0].id, streetcred_mod: -8, cargo: "Bananas", cargo_value: 1000, job_text: bananas},
   {npc_id: allNpcs[0].id, streetcred_mod: -8, cargo: "Logging equipment", cargo_value: 4000, job_text: logging_equip},
   {npc_id: allNpcs[1].id, streetcred_mod: -8, cargo: "Bananas", cargo_value: 1000, job_text: bananas},
   {npc_id: allNpcs[1].id, streetcred_mod: -8, cargo: "Logging equipment", cargo_value: 4000, job_text: logging_equip},
-  # {npc_id: allNpcs[1].id, streetcred_mod: -1, cargo: "Steel pipes", cargo_value: 2000, job_text: steel_pipes},
-  # {npc_id: allNpcs[1].id, streetcred_mod: -1, cargo: "Tractors", cargo_value: 3000, job_text: tractors},
-  # {npc_id: allNpcs[2].id, streetcred_mod: 3, cargo: "Stereo Equipment", cargo_value: 2000, job_text: stereo_equipment},
-  # {npc_id: allNpcs[2].id, streetcred_mod: 3, cargo: "80s Memorabilia", cargo_value: 1500, job_text: eighties_memorabilia},
-  # {npc_id: allNpcs[3].id, streetcred_mod: 6, cargo: "Donated car parts", cargo_value: 8000, job_text: car_parts},
-  {npc_id: allNpcs[2].id, streetcred_mod: 6, cargo: "Used jet skis", cargo_value: 8000, job_text: jet_skis},
-  {npc_id: allNpcs[2].id, streetcred_mod: 8, cargo: "Duffel Bags", cargo_value: 14000, job_text: duffel_bags},
-  {npc_id: allNpcs[3].id, streetcred_mod: 6, cargo: "Used jet skis", cargo_value: 8000, job_text: jet_skis},
-  {npc_id: allNpcs[3].id, streetcred_mod: 8, cargo: "Duffel Bags", cargo_value: 14000, job_text: duffel_bags},
+  {npc_id: allNpcs[2].id, streetcred_mod: 2, cargo: "Expedition", cargo_value: 5000, job_text: expedition},
+  {npc_id: allNpcs[2].id, streetcred_mod: 5, cargo: "Stereo Equipment", cargo_value: 3500, job_text: stereo_equipment},
+  {npc_id: allNpcs[3].id, streetcred_mod: 7, cargo: "80s Memorabilia", cargo_value: 9000, job_text: eighties_memorabilia},
+  {npc_id: allNpcs[3].id, streetcred_mod: 6, cargo: "Donated car parts", cargo_value: 8000, job_text: car_parts},
+  {npc_id: allNpcs[4].id, streetcred_mod: 6, cargo: "Used jet skis", cargo_value: 8000, job_text: jet_skis},
+  {npc_id: allNpcs[4].id, streetcred_mod: 8, cargo: "Duffel Bags", cargo_value: 14000, job_text: duffel_bags},
+  {npc_id: allNpcs[5].id, streetcred_mod: 6, cargo: "Used jet skis", cargo_value: 8000, job_text: jet_skis},
+  {npc_id: allNpcs[5].id, streetcred_mod: 8, cargo: "Duffel Bags", cargo_value: 14000, job_text: duffel_bags},
 ]
 
 all_jobs = jobs.map {|job| Job.create(job)}
@@ -133,25 +131,45 @@ job_options = [
   {job_id: all_jobs[3].id, morality: -3, criminality: 3, option_text: "Oh... That was awkward. I thought you were talking to someone behind me who cares. *You casually turn and walk away*"},
   {job_id: all_jobs[3].id, morality: 10, criminality: 0, option_text: "This stuff is going towards building schools? Why didn't you say? This one's on me, friend."},
 
-  {job_id: all_jobs[4].id, morality: -5, criminality: 8, option_text: "I didn't see anything... I will be out getting pancakes later. That would be a good time to load the cargo."},
-  {job_id: all_jobs[4].id, morality: -8, criminality: 8, option_text: "Bro, I have a tee time in like 30 minutes. Can you get to the point? *you fan through the 100-stack you've been fiddling with*"},
-  {job_id: all_jobs[4].id, morality: 10, criminality: -10, option_text: "You're that guy I saw on 60 Minutes. I'm calling the police."},
-  {job_id: all_jobs[4].id, morality: -10, criminality: 5, option_text: "I know who you are. You're a legend, man. This one's on the house."},
+  {job_id: all_jobs[4].id, morality: 0, criminality: 1, option_text: "Damn, bro. You look like that guy in all the archeology movies. I'm in."},
+  {job_id: all_jobs[4].id, morality: -2, criminality: 2, option_text: "Not so fast bud. You seem like the type that could use a lighter wallet."},
+  {job_id: all_jobs[4].id, morality: -5, criminality: 0, option_text: "I don't like snakes. Get lost."},
+  {job_id: all_jobs[4].id, morality: 3, criminality: 1, option_text: "I've always wanted to go on an adventure. This shipment is on the house. Maybe you can return the favor."},
 
-  {job_id: all_jobs[5].id, morality: -5, criminality: 8, option_text: "This reminds me of the time when I did something I shouldn't have, and it ended up paying off bigtime.  I'm in."},
-  {job_id: all_jobs[5].id, morality: -8, criminality: 8, option_text: "I think im good man, unless..."},
-  {job_id: all_jobs[5].id, morality: 10, criminality: -10, option_text: "I have just the thing for this... *You go to the back room and call the authorities*"},
-  {job_id: all_jobs[5].id, morality: -10, criminality: 5, option_text: "Your money is no good here. Let's get this stuff on the boat."},
+  {job_id: all_jobs[5].id, morality: -2, criminality: 8, option_text: "Yeah man, we deal with stereo equipment a lot in this business. I think we can help you out."},
+  {job_id: all_jobs[5].id, morality: -5, criminality: 8, option_text: "Are you going to pay me the extra %50 speaker fee?"},
+  {job_id: all_jobs[5].id, morality: 7, criminality: -7, option_text: "Someone stole the Blaupunkt out of my hatchback the other day! *As you chase the person while waving a hammer*"},
+  {job_id: all_jobs[5].id, morality: -1, criminality: 13, option_text: "You have the new GC8400 XL 7000 preamp?!?!?! Can we be friends? How about this one's on the house? Wait, what happened to the packaging?"},
 
-  {job_id: all_jobs[6].id, morality: -5, criminality: 8, option_text: "I didn't see anything... I will be out getting pancakes later. That would be a good time to load the cargo."},
-  {job_id: all_jobs[6].id, morality: -8, criminality: 8, option_text: "Bro, I have a tee time in like 30 minutes. Can you get to the point? *you fan through the 100-stack you've been fiddling with*"},
-  {job_id: all_jobs[6].id, morality: 10, criminality: -10, option_text: "You're that guy I saw on 60 Minutes. I'm calling the police."},
-  {job_id: all_jobs[6].id, morality: -10, criminality: 5, option_text: "I know who you are. You're a legend, man. This one's on the house."},
+  {job_id: all_jobs[6].id, morality: -4, criminality: 9, option_text: "Something about this just doesn't feel right. I will do it just because that white suit is so badass."},
+  {job_id: all_jobs[6].id, morality: -7, criminality: 9, option_text: "What the ^%&* kind of car is that you're driving? If you want the best shipping, you're going to have to do the best price..."},
+  {job_id: all_jobs[6].id, morality: 8, criminality: -8, option_text: "I saw someone that looked like you on a documentary about a bunch of drug dealers."},
+  {job_id: all_jobs[6].id, morality: -5, criminality: 5, option_text: "Look, I don't want to be involved with whatever is going on here. Just put it on the boat, but I don't want your money."},
 
-  {job_id: all_jobs[7].id, morality: -5, criminality: 8, option_text: "This reminds me of the time when I did something I shouldn't have, and it ended up paying off bigtime.  I'm in."},
-  {job_id: all_jobs[7].id, morality: -8, criminality: 8, option_text: "I think im good man, unless..."},
-  {job_id: all_jobs[7].id, morality: 10, criminality: -10, option_text: "I have just the thing for this... *You go to the back room and call the authorities*"},
-  {job_id: all_jobs[7].id, morality: -10, criminality: 5, option_text: "Your money is no good here. Let's get this stuff on the boat."},
+  {job_id: all_jobs[7].id, morality: -5, criminality: 8, option_text: "*You chuckle* Hey man, I don't wanna know... You should probably load those at night."},
+  {job_id: all_jobs[7].id, morality: -5, criminality: 8, option_text: "I really like shipping cars, but I like money better..."},
+  {job_id: all_jobs[7].id, morality: 7, criminality: -7, option_text: "*You press a button under the counter to make a bunch of mercenaries seemingly appear from nowhere*"},
+  {job_id: all_jobs[7].id, morality: -4, criminality: 5, option_text: "How about I just take one of those coupes instead of cash?"},
+
+  {job_id: all_jobs[8].id, morality: -5, criminality: 8, option_text: "I didn't see anything... I will be out getting pancakes later. That would be a good time to load the cargo."},
+  {job_id: all_jobs[8].id, morality: -8, criminality: 8, option_text: "Bro, I have a tee time in like 30 minutes. Can you get to the point? *you fan through the 100-stack you've been fiddling with*"},
+  {job_id: all_jobs[8].id, morality: 10, criminality: -10, option_text: "You're that guy I saw on 60 Minutes. I'm calling the police."},
+  {job_id: all_jobs[8].id, morality: -10, criminality: 5, option_text: "I know who you are. You're a legend, man. This one's on the house."},
+
+  {job_id: all_jobs[9].id, morality: -5, criminality: 8, option_text: "This reminds me of the time when I did something I shouldn't have, and it ended up paying off bigtime.  I'm in."},
+  {job_id: all_jobs[9].id, morality: -8, criminality: 8, option_text: "I think im good man, unless..."},
+  {job_id: all_jobs[9].id, morality: 10, criminality: -10, option_text: "I have just the thing for this... *You go to the back room and call the authorities*"},
+  {job_id: all_jobs[9].id, morality: -10, criminality: 5, option_text: "Your money is no good here. Let's get this stuff on the boat."},
+
+  {job_id: all_jobs[10].id, morality: -5, criminality: 8, option_text: "I didn't see anything... I will be out getting pancakes later. That would be a good time to load the cargo."},
+  {job_id: all_jobs[10].id, morality: -8, criminality: 8, option_text: "Bro, I have a tee time in like 30 minutes. Can you get to the point? *you fan through the 100-stack you've been fiddling with*"},
+  {job_id: all_jobs[10].id, morality: 10, criminality: -10, option_text: "You're that guy I saw on 60 Minutes. I'm calling the police."},
+  {job_id: all_jobs[10].id, morality: -10, criminality: 5, option_text: "I know who you are. You're a legend, man. This one's on the house."},
+
+  {job_id: all_jobs[11].id, morality: -5, criminality: 8, option_text: "This reminds me of the time when I did something I shouldn't have, and it ended up paying off bigtime.  I'm in."},
+  {job_id: all_jobs[11].id, morality: -8, criminality: 8, option_text: "I think im good man, unless..."},
+  {job_id: all_jobs[11].id, morality: 10, criminality: -10, option_text: "I have just the thing for this... *You go to the back room and call the authorities*"},
+  {job_id: all_jobs[11].id, morality: -10, criminality: 5, option_text: "Your money is no good here. Let's get this stuff on the boat."},
 ]
 
 all_job_options = job_options.map {|option| JobOption.create(option)}
